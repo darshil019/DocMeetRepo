1d6ecf7d766fa3c654c824b2cf53475300806c5e
darshil019 <panditdarshil5454@gmail.com>
2025-05-15 16:23:44 +0530

    newone added

--- DocMeet/src/App.jsx
+++ DocMeet/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Common/Navbar";
 import SignupPage from "./Components/Common/SignupPage";
+import SignInPage from "./Components/Common/SignInPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <SignupPage/>
+      {/* <SignInPage/> */}
     </div>
   );
 }
