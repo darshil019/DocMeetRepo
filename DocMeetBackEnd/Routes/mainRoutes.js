8cbe39f4a397dcd45390b0d260bce82a9e5e9f72
namrabshah <namrashah56@gmail.com>
2025-05-21 11:37:39 +0530

    .

--- DocMeetBackEnd/Routes/mainRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-let router = express.Router()
-
-const authController = require('../Controllers/authController')
-
-//authController SignUp
-router.post('/user/signup',authController.userSignUp)
-
-module.exports = router
--- DocMeetBackEnd/dbconfig.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://panditdarshil5454:sVr3ha3RkYF8iGfw@darshil.0okpl.mongodb.net/?retryWrites=true&w=majority&appName=Darshil')
-.then(()=>{
-    console.log("DATABASE CONNECTED SUCCESSFULLY")
-})
-.catch((err)=>{
-    console.log("DATABASE NOT CONNECTED SUCCESSFULLY",err)
-})
