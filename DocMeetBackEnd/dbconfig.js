ed8b39be67e724b6eb0f9773df3361e8ccd0d8bc
darshil019 <panditdarshil5454@gmail.com>
2025-05-31 17:09:26 +0530

    Update dbconfig.js

--- DocMeetBackEnd/dbconfig.js
+++ DocMeetBackEnd/dbconfig.js
@@ -1,10 +1,9 @@
-let mongoose=require('mongoose')
+let mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://krishimscit21:ipvudqVTC21DU3M1@cluster0.cxg5w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-
+mongoose.connect('mongodb+srv://panditdarshil5454:sVr3ha3RkYF8iGfw@darshil.0okpl.mongodb.net/?retryWrites=true&w=majority&appName=Darshil')
 .then(()=>{
-    console.log("Database Connected Successfully")
+    console.log("DATABASE CONNECTED SUCCESSFULLY")
 })
 .catch((err)=>{
-    console.log("Database Not Connected",err)
+    console.log("DATABASE NOT CONNECTED SUCCESSFULLY",err)
 })
