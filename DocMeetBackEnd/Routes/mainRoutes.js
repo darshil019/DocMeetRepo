0b1278a6abb4c612b86cc226f347110a1d4f742e
KrishModi <modikrish1964@gmail.com>
2025-06-04 11:00:18 +0530

    adminsigninadded

--- DocMeetBackEnd/Routes/mainRoutes.js
+++ DocMeetBackEnd/Routes/mainRoutes.js
@@ -2,9 +2,11 @@ const express = require('express')
 let router = express.Router()
 
 const authController = require('../Controllers/authController')
+const adminController = require('../Controllers/adminController')
 
 //authController SignUp
 router.post('/user/signup',authController.userSignUp)
 router.post('/user/signin',authController.userSignin)
+router.post('/admin/signin',adminController.adminSignin)
 
 module.exports = router
