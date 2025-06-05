7eebd6db8357b033a70d394fc284f48daaf84759
darshil019 <panditdarshil5454@gmail.com>
2025-06-05 14:48:29 +0530

    .

--- DocMeetBackEnd/dbconfig.js
+++ DocMeetBackEnd/dbconfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb+srv://panditdarshil5454:sVr3ha3RkYF8iGfw@darshil.0okpl.mongodb.net/?retryWrites=true&w=majority&appName=Darshil')
 .then(()=>{
-    console.log("DATABASE SUCCESSFULLY CONNECTED")
+    console.log("DATABASE SUCCESSFTED")
 })
 .catch(()=>{
     console.log("DATABASE NOT SUCCESSFULLY CONNECTED")
